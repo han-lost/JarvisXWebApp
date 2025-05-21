a243375f12c6262176ea8e2f543797fd06e3df1e
han-lost <takasushan@gmail.com>
2025-05-21 20:01:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-import os
-from flask import Flask, request, render_template
 import telebot
 
-# Flask-приложение
-app = Flask(__name__)
-admin_password = "jarvispass"
-latest_signal = "Нажми кнопку, чтобы получить сигнал."
-
-# Telegram Bot и Webhook
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TOKEN = "8051188881:AAHbGSaljlNC5YASV5Jj3BheqEi27PaL0EU"
 bot = telebot.TeleBot(TOKEN)
 
-# Telegram: /start команда
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start(message):
     bot.send_message(message.chat.id, """
 👋 Приветствуем тебя в JarvisXBot!
 
@@ -27,39 +18,7 @@ def send_welcome(message):
 
 Удачи, чемпион!  
 — Твой Джарвис
-""", parse_mode="Markdown")
+"""
+)
 
-# Webhook обработка
-@app.route(f"/{TOKEN}", methods=["POST"])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
-    bot.process_new_updates([update])
-    return "!", 200
-
-# Главная страница
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        return render_template("index.html", signal=latest_signal)
-    return render_template("index.html", signal=None)
-
-# Админ-панель
-@app.route("/admin", methods=["GET", "POST"])
-def admin():
-    global latest_signal
-    if request.method == "POST":
-        if request.form.get("password") == admin_password:
-            signal = request.form.get("signal")
-            if signal:
-                latest_signal = signal
-            return render_template("admin.html", success=True, signal=latest_signal)
-        else:
-            return render_template("admin.html", error="Неверный пароль")
-    return render_template("admin.html")
-
-# Запуск приложения
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=f"https://jarvisx-web.onrender.com/{TOKEN}")
-    app.run(host="0.0.0.0", port=10000)
-
+bot.polling()
