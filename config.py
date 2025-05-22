502c362196460ffe134a6f35a8661fb5d1bd5f44
han-lost <takasushan@gmail.com>
2025-05-22 12:29:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
-TELEGRAM_API_KEY = "ваш_telegram_bot_token"
-ADMIN_PASSWORD = "your_secure_password"
-PARTNER_LINK = "https://onewin.site/promo?code=ВАШ_ПРОМОКОД"
+TOKEN = "8051188881:AAHbGSaljlNC5YASV5Jj3BheqEi27PaL0EU"
+ADMIN_ID = "123456789"  # Замените на ваш Telegram ID
+LINK = "https://goo.su/qnkvtL"
+PROMO = "FXX86"
+
+START_MESSAGE = f"""
+Добро пожаловать в JarvisXBot!
+
+Переходи по ссылке: {LINK}
+Вводи промокод: {PROMO}
+Удачи!
+"""
