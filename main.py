b162cce63dbfa1f94c58054d7190c6df03ad4651
han-lost <takasushan@gmail.com>
2025-05-22 14:03:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def start_handler(message):
     markup.add(telebot.types.InlineKeyboardButton("Перейти на сайт", url=LINK))
     bot.send_message(chat_id, START_MESSAGE, reply_markup=markup)
 
-@bot.message_handler(func=lambda msg: str(msg.chat.id) == ADMIN_ID and msg.text.startswith("/signal"))
+@bot.message_handler(func=lambda msg: msg.chat.id == ADMIN_ID and msg.text.startswith("/signal"))
 def signal_handler(message):
     bot.send_message(message.chat.id, "Сигнал: X10 скоро!")
 
