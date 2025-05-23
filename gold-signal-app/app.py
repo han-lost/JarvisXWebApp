2956cca7f6aabe4a357457af10e94789734999d5
han-lost <takasushan@gmail.com>
2025-05-23 08:14:49 +0200

    Add files via upload

---
+++ gold-signal-app/app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, request, jsonify, abort
+from config import ADMIN_TOKEN
+from logic import strategy_gold
+from logic.strategy_legendary import legendary_strategy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Gold Signal App работает."
+
+@app.route('/admin-signal')
+def admin_signal():
+    token = request.args.get("token")
+    if token != ADMIN_TOKEN:
+        abort(403)
+    signal = strategy_gold.generate_signal()
+    return jsonify({"signal": signal})
+
+@app.route('/legendary')
+def legendary():
+    token = request.args.get("token")
+    if token != ADMIN_TOKEN:
+        abort(403)
+    history = [1.01, 1.25, 2.0, 6.5, 1.4, 1.35]
+    result = legendary_strategy(history)
+    return jsonify(result)
